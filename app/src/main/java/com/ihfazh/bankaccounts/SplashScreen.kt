3b91b4457203d043d24b22c67a81dae2cdff1e2d
ihfazhillah <mihfazhillah@gmail.com>
2021-05-18 18:39:09 +0700

    add text

---
+++ app/src/main/java/com/ihfazh/bankaccounts/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.ihfazh.bankaccounts
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
